1f997cfac219a380d97159be992b6000e58474a1
jannneee <35342454+jannneee@users.noreply.github.com>
2018-12-20 09:11:16 +0500

    Г

---
+++ tests/unit/LoginFormCheckTest.php
@@ -0,0 +1,39 @@
+<?php 
+class LoginFormCheckTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    public function testLoginNoUser()
+    {
+        $this->model = new \app\models\LoginForm([
+            'username' => 'not_existing_username',
+            'password' => 'not_existing_password',
+        ]);
+
+        expect_not($this->model->login());
+
+    }
+
+    public function testLoginWrongPassword()
+    {
+        $this->model = new \app\models\LoginForm([
+            'username' => 'lol@yandex.ru',
+            'password' => 'wrong_password',
+        ]);
+
+        expect_not($this->model->login());
+
+    }
+
+    public function testLoginCorrect()
+    {
+        $this->model = new \app\models\LoginForm([
+            'username' => 'lol@yandex.ru',
+            'password' => '123',
+        ]);
+
+        expect($this->model->login());
+
+    }
+}
